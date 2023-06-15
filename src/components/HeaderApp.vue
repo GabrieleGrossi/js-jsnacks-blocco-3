d48f625bab21211fd036b1668b7818e6c148f931
GabrieleGrossi <grossigabriele98@gmail.com>
2023-06-15 12:53:00 +0200

    final push

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <h1>
+            Header app for snack
+        </h1>
+    </header>
+</template>
+<script>
+export default {
+    name:'HeaderApp',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style>
+</style>
