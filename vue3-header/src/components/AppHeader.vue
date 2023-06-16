7db43d8ded6f263ef1317d3dca8ace2ef470d2c0
GabrieleGrossi <grossigabriele98@gmail.com>
2023-06-16 12:37:35 +0200

    commit for correction

---
+++ vue3-header/src/components/AppHeader.vue
@@ -0,0 +1,52 @@
+<template lang="">
+    <div>
+        <ul>
+            <AppHeaderLink v-for="liElement in liList"
+            :navname = liElement.name
+            :link = liElement.link />
+        </ul>
+    </div>
+</template>
+<!-- <script> -->
+<script>
+import AppHeaderLink from './AppHeaderLink.vue';
+
+export default {
+    components: {
+        AppHeaderLink,
+    },
+
+    name: 'AppHeader',
+
+    data() {
+        return {
+            nav: [
+                {
+                    name: 'A',
+                    link: '#'
+                },
+                {
+                    name: 'B',
+                    link: '#'
+                },
+                {
+                    name: 'C',
+                    link: '#'
+                },
+                {
+                    name: 'D',
+                    link: '#'
+                },
+                {
+                    name: 'E',
+                    link: '#'
+                },
+            ]
+        }
+    },
+    methods() {
+    },
+}		
+</script>
+
+<style lang="scss"></style> 
